c4928d0d1d93c8e9f5e1faa1303589a31f12fd5b
Kazoroo <91783342+Kazoroo@users.noreply.github.com>
2022-08-19 21:03:10 +0200

    Update main.py
    Downloading words from .txt and coloring text

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
 from random import choice
 from time import time
 from math import ceil
-
-words = ["kokos", "Nairobi", "cytryna", "Jowisz", "Adam",
-         "Afryka", "Polska", "banan", "Ateny", "Barbara",
-         "Azja", "gość", "zły", "szczęśliwy", "marynarz",
-         "wściekły", "przyjaciel", "pies", "żółw", "zupa",
-         "statek", "Argentyna", "kowal", "rymarz", "rynek",
-         "wóz", "Andrzej", "kochany", "idealny", "zielony",
-         "jest", "tak", "nie", "dlaczego", "lub", "wschód",
-         "Europa", "Niemcy", "karabin", "granat", "czerwony",
-         "mango", "Francja", "limonka", "Mars", "Mateusz",
-         "gruz", "przyczepa", "Barbados", "Warszawa", "Moskwa",
-         "Benin", "start", "koniec", "książka", "bosman",
-         "głupi", "mądry", "lód", "śnieg", "co",
-         "twarz", "jelita", "noga", "skóra", "krupier",
-         "kasyno", "neon", "prezydent", "jadalny", "las",
-         "lis", "samolot", "lotnisko", "wiatr", "fale", "kosmos",
-         "Belize", "dzięcioł", "trzcina", "łopata", "telefon"]
+from termcolor import colored
 
 print(" " * 2)
 
@@ -25,13 +9,21 @@ time_start = time()
 correct_answers = 0
 loop_end = 0
 
+with open('words.txt', 'r', encoding='utf-8') as f:
+    words = [line.strip() for line in f]
 
 while time() < time_start + 60:
     remaining_time = time_start - time() + 60
 
     word = choice(words)  # draws the word
-    print("Zostało ci " + str(ceil(remaining_time)) + " sekund!")
-    print(word)
+
+    if remaining_time > 5:
+        print("Zostało ci " + str(ceil(remaining_time)) + " sekund!")
+        print(word)
+    else:
+        print(colored("Zostało ci " + str(ceil(remaining_time)) + " sekund!", 'red'))
+        print(word)
+
     answer = input("Przepisz powyższe wyrazy: ")
     words.remove(word)  # deleting used words
     loop_end += 1
